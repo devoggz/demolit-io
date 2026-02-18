4f91f5c03fc3deb4701b51df12c6bb1cb7dab469
Voggz <49722299+devoggz@users.noreply.github.com>
2026-02-18 22:43:44 +0300

    updates

--- components/Common/ProductItem.tsx
+++ components/Common/ProductItem.tsx
@@ -20,173 +20,165 @@ import { formatPrice } from "@/utils/formatePrice";
 import { calculateDiscountPercentage } from "@/utils/calculateDiscountPercentage";
 
 type Props = {
-  bgClr?: string;
-  item: Product;
+    bgClr?: string;
+    item: Product;
 };
 
 const ProductItem = ({ item, bgClr = "[#F6F7FB]" }: Props) => {
-  const defaultVariant = item?.productVariants.find(
-    (variant) => variant.isDefault,
-  );
+    const defaultVariant = item?.productVariants.find(
+        (variant) => variant.isDefault,
+    );
 
-  const { openModal } = useModalContext();
-  const dispatch = useDispatch<AppDispatch>();
-  const { addItem, cartDetails } = useCart();
-  const router = useRouter();
+    const { openModal } = useModalContext();
+    const dispatch = useDispatch<AppDispatch>();
+    const { addItem, cartDetails } = useCart();
+    const router = useRouter();
 
-  const isAlreadyAdded = Object.values(cartDetails ?? {}).some(
-    (cartItem) => cartItem.id === item.id,
-  );
+    const isAlreadyAdded = Object.values(cartDetails ?? {}).some(
+        (cartItem) => cartItem.id === item.id,
+    );
 
-  const cartItem = {
-    id: item.id,
-    name: item.title,
-    price: item.discountedPrice ? item.discountedPrice : item.price,
-    currency: "kes",
-    image: defaultVariant?.image ? defaultVariant.image : "",
-    slug: item?.slug,
-    availableQuantity: item.quantity,
-    color: defaultVariant?.color ? defaultVariant.color : "",
-    size: defaultVariant?.size ? defaultVariant.size : "",
-  };
-
-  const productUrl = `/products/${item?.slug}`;
-
-  const handleQuickViewUpdate = () => {
-    const serializableItem = {
-      ...item,
-      updatedAt:
-        typeof item.updatedAt === "string"
-          ? item.updatedAt
-          : item.updatedAt.toISOString(),
+    const cartItem = {
+        id: item.id,
+        name: item.title,
+        price: item.discountedPrice ? item.discountedPrice : item.price,
+        currency: "kes",
+        image: defaultVariant?.image ? defaultVariant.image : "",
+        slug: item?.slug,
+        availableQuantity: item.quantity,
+        color: defaultVariant?.color ? defaultVariant.color : "",
+        size: defaultVariant?.size ? defaultVariant.size : "",
     };
 
-    dispatch(updateQuickView(serializableItem));
-  };
+    const productUrl = `/products/${item?.slug}`;
 
-  const handleAddToCart = (item: Product) => {
-    if (item.quantity > 0) {
-      // @ts-ignore
-      addItem(cartItem);
-      toast.success("Product added to cart!");
-    } else {
-      toast.error("This product is out of stock!");
-    }
-  };
+    const handleQuickViewUpdate = () => {
+        const serializableItem = {
+            ...item,
+            updatedAt:
+                typeof item.updatedAt === "string"
+                    ? item.updatedAt
+                    : item.updatedAt.toISOString(),
+        };
 
-  const handleItemToWishList = () => {
-    dispatch(
-      addItemToWishlist({
-        id: item.id,
-        title: item.title,
-        slug: item.slug,
-        image: defaultVariant?.image ? defaultVariant.image : "",
-        price: item.discountedPrice ? item.discountedPrice : item.price,
-        quantity: item.quantity,
-        color: defaultVariant?.color ? defaultVariant.color : "",
-      }),
-    );
-  };
+        dispatch(updateQuickView(serializableItem));
+    };
 
-  return (
-    <article className="group">
-      {/* Clickable card header */}
-      <button
-        type="button"
-        className="w-full text-left cursor-pointer"
-        onClick={() => router.push(productUrl)}
-      >
-        <div
-          className={`relative overflow-hidden border border-gray-3 flex items-center justify-center rounded-xl bg-${bgClr} min-h-[270px] mb-4`}
-        >
-          <Image
-            alt={item.title || "product-image"}
-            height={250}
-            src={defaultVariant?.image ? defaultVariant.image : ""}
-            width={250}
-          />
+    const handleAddToCart = (item: Product) => {
+        if (item.quantity > 0) {
+            // @ts-ignore
+            addItem(cartItem);
+            toast.success("Product added to cart!");
+        } else {
+            toast.error("This product is out of stock!");
+        }
+    };
 
-          <div className="absolute top-2 right-2">
-            {item.quantity < 1 ? (
-              <span className="px-2 py-1 text-xs font-medium text-white bg-red-500 rounded-full">
+    const handleItemToWishList = () => {
+        dispatch(
+            addItemToWishlist({
+                id: item.id,
+                title: item.title,
+                slug: item.slug,
+                image: defaultVariant?.image ? defaultVariant.image : "",
+                price: item.discountedPrice ? item.discountedPrice : item.price,
+                quantity: item.quantity,
+                color: defaultVariant?.color ? defaultVariant.color : "",
+            }),
+        );
+    };
+
+    return (
+        <article className="group">
+            {/* Clickable card header */}
+            <div
+                className="w-full text-left cursor-pointer"
+                onClick={() => router.push(productUrl)}
+            >
+                <div
+                    className={`relative overflow-hidden border border-gray-3 flex items-center justify-center rounded-xl bg-${bgClr} min-h-[270px] mb-4`}
+                >
+                    <Image
+                        alt={item.title || "product-image"}
+                        height={250}
+                        src={defaultVariant?.image ? defaultVariant.image : ""}
+                        width={250}
+                    />
+
+                    <div className="absolute top-2 right-2">
+                        {item.quantity < 1 ? (
+                            <span className="px-2 py-1 text-xs font-medium text-white bg-red-500 rounded-full">
                 Out of Stock
               </span>
-            ) : item?.discountedPrice && item?.discountedPrice > 0 ? (
-              <span className="px-2 py-1 text-xs font-medium text-white rounded-full bg-green-bright">
+                        ) : item?.discountedPrice && item?.discountedPrice > 0 ? (
+                            <span className="px-2 py-1 text-xs font-medium text-white rounded-full bg-green-bright">
                 {calculateDiscountPercentage(item.discountedPrice, item.price)}%
                 OFF
               </span>
-            ) : null}
-          </div>
+                        ) : null}
+                    </div>
 
-          <div className="absolute left-0 bottom-0 translate-y-full w-full flex items-center justify-center gap-2.5 pb-5 ease-linear duration-200 group-hover:translate-y-0">
-            <Tooltip content="Quick View" placement="top">
-              <button
-                type="button"
-                className="border border-gray-3 h-[38px] w-[38px] rounded-lg flex items-center justify-center text-dark-6 bg-white hover:text-green-bright"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  openModal();
-                  handleQuickViewUpdate();
-                }}
-              >
-                <EyeIcon />
-              </button>
-            </Tooltip>
+                    <div className="absolute left-0 bottom-0 translate-y-full w-full flex items-center justify-center gap-2.5 pb-5 ease-linear duration-200 group-hover:translate-y-0">
+                        <Tooltip content="Quick View" placement="top">
+                            <button
+                                type="button"
+                                className="border border-gray-3 h-[38px] w-[38px] rounded-lg flex items-center justify-center text-dark-6 bg-white hover:text-green-bright"
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    openModal();
+                                    handleQuickViewUpdate();
+                                }}
+                            >
+                                <EyeIcon />
+                            </button>
+                        </Tooltip>
 
-            {isAlreadyAdded ? (
-              <button
-                type="button"
-                onClick={(e) => e.stopPropagation()}
-                className="p-0"
-              >
-                <CheckoutBtn />
-              </button>
-            ) : (
-              <button
-                type="button"
-                className="inline-flex px-5 py-2 font-medium h-[38px] text-white duration-200 ease-out rounded-lg text-custom-sm bg-dark-6 hover:bg-green-bright"
-                disabled={item.quantity < 1}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleAddToCart(item);
-                }}
-              >
-                {item.quantity > 0 ? "Add to Cart" : "Out of Stock"}
-              </button>
-            )}
+                        {isAlreadyAdded ? (
+                            // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+                            <div onClick={(e) => e.stopPropagation()}>
+                                <CheckoutBtn />
+                            </div>
+                        ) : (
+                            <button
+                                type="button"
+                                className="inline-flex px-5 py-2 font-medium h-[38px] text-white duration-200 ease-out rounded-lg text-custom-sm bg-dark-6 hover:bg-green-bright"
+                                disabled={item.quantity < 1}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    handleAddToCart(item);
+                                }}
+                            >
+                                {item.quantity > 0 ? "Add to Cart" : "Out of Stock"}
+                            </button>
+                        )}
 
-            {/* wishlist button */}
-            <button
-              type="button"
-              onClick={(e) => e.stopPropagation()}
-              className="p-0"
-            >
-              <WishlistButton
-                handleItemToWishList={handleItemToWishList}
-                item={item}
-              />
-            </button>
-          </div>
-        </div>
-      </button>
+                        {/* wishlist button */}
+                        <div onClick={(e) => e.stopPropagation()}>
+                            <WishlistButton
+                                handleItemToWishList={handleItemToWishList}
+                                item={item}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-      <h3 className="font-semibold text-dark-6 ease-out text-base duration-200 hover:text-green-bright mb-1.5 line-clamp-1">
-        {item.title}
-      </h3>
+            <h3 className="font-semibold text-dark-6 ease-out text-base duration-200 hover:text-green-bright mb-1.5 line-clamp-1">
+                {item.title}
+            </h3>
 
-      <span className="flex items-center gap-2 text-base font-medium">
+            <span className="flex items-center gap-2 text-base font-medium">
         {item.discountedPrice && (
-          <span className="line-through text-dark-4">
+            <span className="line-through text-dark-4">
             {formatPrice(item.price)}
           </span>
         )}
-        <span className="text-dark-6">
+                <span className="text-dark-6">
           {formatPrice(item.discountedPrice || item.price)}
         </span>
       </span>
-    </article>
-  );
+        </article>
+    );
 };
 
 export default ProductItem;
